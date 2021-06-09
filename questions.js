fa2d73f703b673a3a4ebe58d1d30b4cd01aeaa4d
codingyou <codingyou@naver.com>
2021-06-09 21:49:22 +0900

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,633 @@
+questions = [
+    {
+        "question": "기업에 의한 억압은 정부에 의한 억압보다 더욱 우려스럽다.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "정부가 소비자를 보호하기 위해 경제에 개입할 필요가 있다.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "시장이 자유로워질수록, 사람들도 더 자유로워진다.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "모든 시민의 복지를 보장하는 것보다 균형 잡힌 예산을 유지하는 것이 낫다.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "공적 연구는 시장에 맡기는 것보다 국민에게 더 유익하다.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "국제 무역에 대한 관세는 국내 생산을 장려하기 위해 중요하다.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "능력에​ ​따라​ ​일하고​ ​필요에​ 따른 분배를 해야 한다.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "사회 복지가 폐지되고 민간 자선사업이 이를 대체하는 것이 최선일 것이다.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "부자들에 증세하여 그 돈을 가난한 사람들에게 제공해야 한다.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "상속은 합법적인 형태의 부(富)이다.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "도로나​ ​전기와 같은 ​기초재는​ ​공영화되어야​ ​한다.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "정부의 개입은 경제에 위협이 된다.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "더​ ​많은​ ​돈을​ ​지불할​ ​능력이​ ​있는​ ​사람들은​ ​더​ ​좋은​ 의료 서비스를 ​받을​ ​수​ ​있어야​ ​한다.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "양질의​ ​교육은​ ​모든​ ​사람들의​ ​권리이다.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "생산수단은 그것을 사용하는 노동자의 소유여야 한다.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "유엔(UN)은 폐지되어야 한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "우리나라를 지키기 위한 군사 작전은 때때로 필수불가결하다.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "나는 유럽연합(EU)과 같은 지역 연합을 지지한다.",
+        "effect": {
+            "econ": -5,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 5
+        }
+    },
+    {
+        "question": "우리의 국가 주권을 유지하는 것은 중요하다.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "연합된 세계정부는 인류에게 이로울 것이다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "국력 증대보다 (주변국과의) 평화로운 관계 유지가 더 중요하다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "다른 국가들에게 (자국의) 전쟁을 정당화 할 필요는 없다.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "군사 비용(군비)은 돈 낭비이다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "국제 원조는 돈 낭비다.",
+        "effect": {
+            "econ": -5,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "우리나라는​ ​위대하다.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "연구는​ ​국제적​ ​규모로​ ​이루어져야​ ​한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "정부는 국제사회에 책임을 질 수 있어야 한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "권위주의적 정부에 대한 시위일지라도, 폭력 행위는 용납되면 안 된다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "나의​ ​종교적​ ​가치는​ ​가능한​ ​한​ ​널리​ ​퍼져야​ ​한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "우리나라의​ ​가치는​ ​가능한​ ​한​ ​널리​ ​퍼져야​ ​한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "법과​ ​질서를​ ​유지하는​ ​것은​ ​매우​ ​중요하다.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "일반​ ​대중들은​ 잘못된 ​결정을​ ​내린다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "의사 보조 자살은 합법적이어야 한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "테러​ ​행위로부터​ ​우리를​ ​보호하기​ ​위해서는​ ​시민의​ ​자유를​ ​희생시킬 수밖에 없다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "현대​ ​사회에서는​ ​정부의​ ​감시가​ 필요하다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "국가의 존재 자체가 우리의 자유에 대한 위협이다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "정치적​ ​견해와​ ​상관없이​ ​국가의​ ​편을​ ​들어야 한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "모든 권위는 의심해 보아야 한다",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 5
+        }
+    },
+    {
+        "question": "계급 사회가 최고다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "비록 다수의 의견이 잘못되었다 하더라도, 정부는 다수의 의견을 따라야 한다",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "리더십은​ ​강할수록​ ​좋다.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "민주주의는 단순한 의사결정 과정 이상의 가치를 지닌다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "환경 규제는 필수적이다.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "더​ ​나은​ ​세상은​ ​자동화,​ ​과학​ ​및​ ​기술에서​ ​나온다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "아이들은 종교적이거나 전통적인 가치를 교육받아야 한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "전통은​ ​그​ ​자체로는​ ​가치가​ ​없다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "종교는 정부의 활동에 영향을 끼쳐야 한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "종교기관은​ ​다른​ ​기관에​ ​과세하는​ 방식과 동일하게​ ​과세해야​ ​한다.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "기후 변화는 현재 우리의 삶에서 가장 큰 위협 중 하나다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "기후​ ​변화에​ ​대처하기​ ​위해​ ​세계가​ ​하나​ ​되어야 한다",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "예전 사회가 지금 사회보다 좋았다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "과거의​ ​전통을​ ​유지하는​ ​것이​ ​중요하다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "우리​ ​세대를 넘어 장기적으로 생각해야 한다",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "이성(理性)은 우리 문화를 유지하는 것보다 중요하다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "약물 사용은 합법화되거나 비범죄화되어야 한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 2
+        }
+    },
+    {
+        "question": "동성 결혼은 합법이어야 한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 10
+        }
+    },
+    {
+        "question": "어떠한 문화도 다른 문화보다 우월하지 않다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 5,
+            "scty": 10
+        }
+    },
+    {
+        "question": "결혼 이외의 성관계는 부도덕하다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "이주민을 받아들인다면, 그들을 우리 문화에 동화시켜야 하는 것이 중요하다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "임신중절은 대부분 혹은 모든 경우에서 금지되어야 한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "정당한 사유가 없는 총기 소유는 금지해야 한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "나는 단일 지불자, 보편적 건강보장을 지지한다.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "성매매는 불법이어야 한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
+        }
+		
+    },
+    {
+        "question": "전통적인 가족관을 유지하는 것은 중요하다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "가능한 모든 수단을 동원해 진보를 쫒는 것은 위험하다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "유전자 변형은 사람에게 적용될지라도 좋은 영향을 끼칠 것이다",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "우리는 이민자들에게 국경을 개방해야 한다.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "정부는 내국인을 고려하는 만큼, 외국인에게도 신경을 써야 한다.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "모든 사람들은 문화나 성별과 같은 요인에 상관없이 동등하게 대우받아야 한다.",
+        "effect": {
+            "econ": 10,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 10
+        }
+    },
+    {
+        "question": "우리 공동체의 목표를 다른 어떤 공동체보다도 우선시하는 것이 중요하다.",
+        "effect": {
+            "econ": -10,
+            "dipl": -10,
+            "govt": -10,
+            "scty": -10
+        }
+    }
+];
